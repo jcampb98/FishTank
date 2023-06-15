46b68295413b1fd4952e005efdb6f6b6ddbcf112
Joshua Campbell <Caboose768@gmail.com>
2023-06-15 10:50:54 +0100

    Added Console App files via upload

---
+++ FishTankConsole/FishTankConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FishTank;
+
+var tank = new Tank();
+
+var goldFish = new GoldFish("John");
+var angelFish = new AngelFish("Robert");
+var babelFish = new BabelFish("Peter");
+
+tank.Add(goldFish);
+Console.WriteLine(tank.Feed());
+
+tank.Add(angelFish);
+Console.WriteLine(tank.Feed());
+
+tank.Add(babelFish);
+Console.WriteLine(tank.Feed());
+Console.ReadLine();
